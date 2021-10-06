28c09fdcd4ac8e2e8e78774ddf4e6354931bc660
Diego Calle <dnetix@gmail.com>
2021-10-06 13:54:48 -0500

    Iterate contracts instead of mock data

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -9,6 +9,7 @@
                     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                             <table class="min-w-full divide-y divide-gray-200">
+                                <caption class="px-6 py-4 text-left text-gray-500">Contratos recientes</caption>
                                 <thead class="bg-gray-50">
                                 <tr>
                                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -23,21 +24,21 @@
                                 </tr>
                                 </thead>
                                 <tbody class="bg-white divide-y divide-gray-200">
-                                @for($i=0; $i<15; $i++)
+                                @foreach($contracts as $contract)
                                     <tr>
-                                        <td class="px-6 py-4 text-left">
-                                            <div class="text-gray-900">33122</div>
-                                            <div class="text-sm text-gray-500">2021-09-12 08:00:00</div>
+                                        <td class="p-4 text-left">
+                                            <div class="text-gray-900">{{ $contract->id() }}</div>
+                                            <div class="text-xs text-gray-500 whitespace-nowrap">{{ $contract->present()->createdAt() }}</div>
                                         </td>
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            <div class="text-sm text-gray-900">Pulidora Black Decker   G720 B3   ( sin disco )</div>
-                                            <div class="text-sm text-gray-500">Yuber Alberto Osorio Macias</div>
+                                        <td class="p-4">
+                                            <div class="text-sm text-gray-900">{{ $contract->present()->articlesNames() }}</div>
+                                            <div class="text-sm text-gray-500">{{ $contract->present()->clientName() }}</div>
                                         </td>
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            $ 50.000
+                                        <td class="p-4 whitespace-nowrap">
+                                            {{ $contract->present()->amount() }}
                                         </td>
                                     </tr>
-                                @endfor
+                                @endforeach
                                 </tbody>
                             </table>
                         </div>
@@ -52,16 +53,14 @@
                 <div class="px-4 pt-5">
                     <div class="flex items-start">
                         <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-yellow-400">
-                            <i class="fa fa-users"></i>
+                            <i class="fa fa-users text-black"></i>
                         </div>
                         <div class="text-left pl-4">
                             <h3 class="text-lg leading-6 font-medium text-gray-900">
-                                Buscar clientes
-                            </h3>
+                                Buscar clientes </h3>
                             <div class="mt-2">
                                 <p class="text-sm text-gray-500">
-                                    Busqueda por nombre o cedula
-                                </p>
+                                    Busqueda por nombre o cedula </p>
                             </div>
                         </div>
                     </div>
