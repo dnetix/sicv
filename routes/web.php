401479a4219b26ae41c8afdfcb93e4a9bf4e2145
Diego Calle <dnetix@gmail.com>
2021-10-11 17:22:10 -0500

    Add capability to send a contract directly to a client

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => 'auth'], function () {
     Route::post('/contract/store', [\App\Http\Controllers\ContractController::class, 'store'])
         ->name('contract.store');
-    Route::get('/contract/new', [\App\Http\Controllers\ContractController::class, 'create'])
+    Route::get('/contract/new/{client?}', [\App\Http\Controllers\ContractController::class, 'create'])
         ->name('contract.new');
 
     Route::get('/dashboard', [HomeController::class, 'dashboard'])
         ->name('dashboard');
 });
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 require __DIR__ . '/auth.php';
