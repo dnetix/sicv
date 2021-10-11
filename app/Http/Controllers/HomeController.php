3374bdcdc1e426ae743609c960d837ab1a1c8dfc
Diego Calle <dnetix@gmail.com>
2021-10-11 17:24:04 -0500

    Redirect when user is already logged

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,9 @@ class HomeController extends Controller
 {
     public function index(Request $request)
     {
+        if (auth()->user()) {
+            return redirect()->route('dashboard');
+        }
         return view($request->get('view') ?? 'user.login');
     }
 
