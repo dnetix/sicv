bb800e03e26d9fc288ae8f1f0cd11008f5146502
Diego Calle <dnetix@gmail.com>
2022-06-20 23:56:25 -0500

    Add new contract print route

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,15 @@
 <?php
 
+use App\Http\Controllers\ContractController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::post('/contract/store', [\App\Http\Controllers\ContractController::class, 'store'])
-        ->name('contract.store');
-    Route::get('/contract/new/{client?}', [\App\Http\Controllers\ContractController::class, 'create'])
-        ->name('contract.new');
+    Route::post('/contract/store', [ContractController::class, 'store'])->name('contract.store');
+    Route::get('/contract/new/{client?}', [ContractController::class, 'create'])->name('contract.new');
+    Route::get('/contract/print/{contract}', [ContractController::class, 'print'])->name('contract.print');
 
-    Route::get('/dashboard', [HomeController::class, 'dashboard'])
-        ->name('dashboard');
+    Route::get('/dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
 });
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
