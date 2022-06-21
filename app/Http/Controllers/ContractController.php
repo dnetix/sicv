064a127903fb8f2e43d37aeb92b3027a011df2c7
Diego Calle <dnetix@gmail.com>
2022-06-21 00:06:11 -0500

    Basic contract controller

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\RepositoryHelper;
+use App\Http\Requests\ContractCreateRequest;
 use App\Models\Clients\Client;
+use App\Models\Contracts\Actions\CreateNewContractAction;
+use App\Models\Contracts\Contract;
 use Illuminate\Http\Request;
 
 class ContractController extends Controller
@@ -13,6 +16,28 @@ class ContractController extends Controller
         return view('contract.create', [
             'client' => $client,
             'articleTypes' => RepositoryHelper::forArticles()->getArticleTypes(),
+        ]);
+    }
+
+    public function store(ContractCreateRequest $request)
+    {
+        $contract = (new CreateNewContractAction(
+            auth()->user()->getAuthIdentifier(),
+            $request->clientId(),
+            $request->months(),
+            $request->percentage(),
+            $request->articles(),
+            $request->note()
+        ))->execute();
+
+        return redirect(route('contract.print', $contract->id()));
+    }
+
+    public function print(Contract $contract)
+    {
+        return view('contract.print', [
+            'contract' => $contract,
+            'articles' => RepositoryHelper::forContracts()->getContractArticles($contract),
         ]);
     }
 }
