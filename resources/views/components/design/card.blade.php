ca5eee385b6d7ad4941fa3ff421a3c69ad1b515b
Diego Calle <dnetix@gmail.com>
2022-06-22 17:44:06 -0500

    Improve card component

--- resources/views/components/design/card.blade.php
+++ resources/views/components/design/card.blade.php
@@ -1,13 +1,15 @@
 @props(['title' => '', 'subtitle' => ''])
 
 <div>
-    <div class="px-4 py-4 bg-gray-50">
-        <h1 class="text-lg leading-6 font-medium text-gray-900">{{ $title }}</h1>
-        @if($subtitle)
-            <h2 class="mt-1 text-sm text-gray-500">{{ $subtitle }}</h2>
-        @endif
-    </div>
-    <div class="p-4 {{ $attributes->get('body-class') }}">
-        {{ $slot }}
+    <div class="card border border-gray-100 rounded overflow-hidden mb-4">
+        <div class="px-4 py-4 bg-gray-50">
+            <h1 class="text-lg leading-6 font-medium text-gray-900">{{ $title }}</h1>
+            @if($subtitle)
+                <h2 class="mt-1 text-sm text-gray-500">{{ $subtitle }}</h2>
+            @endif
+        </div>
+        <div class="p-4 {{ $attributes->get('body-class') }}">
+            {{ $slot }}
+        </div>
     </div>
 </div>
