3c184ae9bc7a33bcab11cf1005327f9de1ef1163
Diego Calle <dnetix@gmail.com>
2022-06-22 17:44:34 -0500

    Use svg helper instead of svg directly

--- resources/views/contract/create.blade.php
+++ resources/views/contract/create.blade.php
@@ -75,19 +75,13 @@
                         </template>
 
                         <template x-if="!selectedClient && emptyResponse">
-                            <div class="p-4 text-center">
+                            <div class="p-4 text-center bg-gray-100 p-4 mb-4">
                                 <h3 class="text-sm font-medium text-gray-900">No hay clientes que coincidan con la
                                     búsqueda</h3>
                                 <div class="mt-3">
                                     <a href="{{ route('client.new') }}"
                                        class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                        <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg"
-                                             viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                            <path fill-rule="evenodd"
-                                                  d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-                                                  clip-rule="evenodd"/>
-                                        </svg>
-                                        Nuevo Cliente
+                                        @svg('heroicon-o-plus', '-ml-1 mr-2 h-5 w-5') Nuevo Cliente
                                     </a>
                                 </div>
                             </div>
