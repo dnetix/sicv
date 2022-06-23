a475b54a7fab154c74768eb207b3724beace80fa
Diego Calle <dnetix@gmail.com>
2022-06-22 20:48:43 -0500

    Add translations

--- resources/lang/es/client.php
+++ resources/lang/es/client.php
@@ -1,10 +1,22 @@
 <?php
 
 return [
+    'create_title' => 'Nuevo Cliente',
+    'submit' => 'Guardar cliente',
+
+    'name' => 'Nombre',
+    'document_type' => 'Tipo documento',
+    'document' => 'Número',
+    'expedition_city' => 'Ciudad expedición',
+    'mobile' => 'Celular',
+    'email' => 'Correo electrónico',
+    'phone_number' => 'Telefono',
     'search_label' => 'Cliente',
     'search_placeholder' => 'Buscar cliente',
     'empty_search' => 'No hay clientes que coincidan con la búsqueda',
     'new_label' => 'Nuevo Cliente',
+
+    'document_type_cc' => 'Cédula',
 
     'note' => 'Nota',
     'note_importance' => 'Tipo Nota',
