dc54c3bf3627a4859af5c410662087220f0909f6
Diego Calle <dnetix@gmail.com>
2022-06-22 20:45:42 -0500

    Update ClientController.php

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\RepositoryHelper;
+use App\Http\Requests\UserCreateRequest;
+use App\Models\Clients\Actions\CreateNewClientAction;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
@@ -10,7 +12,14 @@ class ClientController extends Controller
     public function create()
     {
         return view('client.create', [
+            // TODO: Define where Im going to store this
+            'documentTypes' => ['CC' => trans('client.document_type_cc')],
         ]);
+    }
+
+    public function store(UserCreateRequest $request)
+    {
+        return (new CreateNewClientAction($request->validated()))->execute();
     }
 
     public function search(Request $request)
