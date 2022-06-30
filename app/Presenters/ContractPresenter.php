191cd3df3013183176ea80e754324a08ada0e367
Diego Calle <dnetix@gmail.com>
2022-06-29 22:28:26 -0500

    Remove useless methods for contract presenter

--- app/Presenters/ContractPresenter.php
+++ app/Presenters/ContractPresenter.php
@@ -4,13 +4,20 @@ namespace App\Presenters;
 
 use App\Helpers\Dates\DateHelper;
 use App\Helpers\RepositoryHelper;
-use App\Models\Contracts\ContractStates;
+use App\Models\Contracts\Contract;
 use App\Models\Utils\Presenters\Presenter;
 
+/**
+ * @property Contract $entity
+ */
 class ContractPresenter extends Presenter
 {
-    public $dueDateHelper;
     public $lastExtension;
+
+    public function clientName(): string
+    {
+        return $this->entity->getClient()->name();
+    }
 
     public function amount()
     {
@@ -22,9 +29,9 @@ class ContractPresenter extends Presenter
         return $this->toMoney($this->entity->endAmount());
     }
 
-    public function payment()
+    public function extension(): string
     {
-        return $this->toMoney($this->entity->payment());
+        return $this->toMoney($this->entity->extension());
     }
 
     public function percentage()
@@ -42,176 +49,29 @@ class ContractPresenter extends Presenter
         return $this->toMoney($this->entity->payedExtensions());
     }
 
-    public function dueDate()
-    {
-        return DateHelper::create($this->entity->createdAt())->changeMonths($this->entity->contractMonths())->translateToHumanDate();
-    }
-
-    public function dueDateDifference()
-    {
-        return DateHelper::create($this->entity->createdAt())->changeMonths($this->entity->contractMonths())->toDifferenceWith()->forHumans();
-    }
-
     public function endDate()
     {
         return DateHelper::create($this->entity->endDate())->translateToHumanDate();
     }
 
-    public function endDateDifference()
-    {
-        return DateHelper::create($this->entity->endDate())->toDifferenceWith()->forHumans();
-    }
-
-    public function duedExtensions()
-    {
-        return $this->toMoney($this->entity->duedExtensions());
-    }
-
-    public function clientName()
-    {
-        return $this->entity->client->name();
-    }
-
-    public function clientContactInfo()
-    {
-        return $this->entity->client->present()->phones();
-    }
-
     public function state()
     {
-        return ContractStates::$FORHUMAN[$this->entity->state()];
+        return trans('contract.' . $this->entity->state());
     }
 
-    public function contractDate()
+    public function date()
     {
         $dateHelper = DateHelper::create($this->entity->createdAt());
         return $dateHelper->translateToHumanDate() . ' ' . $dateHelper->translateToTime();
     }
 
-    public function shortCreatedAt()
-    {
-        return DateHelper::create($this->entity->createdAt())->translateToShortDate();
-    }
-
-    public function elapsedSinceCreated()
-    {
-        return DateHelper::getDifference($this->entity->createdAt())->forHumans();
-    }
-
-    public function elapsedMonths()
-    {
-        $difference = $this->entity->elapsedDifference();
-        if ($difference->inMonths() != 0) {
-            return $difference->inMonths() . (($difference->inMonths() > 1) ? ' meses' : ' mes');
-        } else {
-            return $difference->forHumans();
-        }
-    }
-
-    public function articlesNames()
+    public function articlesNames(): string
     {
         $articles = RepositoryHelper::forContracts()->getContractArticles($this->entity);
         foreach ($articles as $article) {
             $articleNames[] = !empty($article->weight()) ? $article->description() . ' [' . ($article->weight() + 0) . 'g]' : $article->description();
         }
         return implode(', ', $articleNames);
-    }
-
-    private function getlastExtension()
-    {
-        if (is_null($this->lastExtension) && $this->lastExtension !== false) {
-            $this->lastExtension = $this->entity->lastExtension();
-            if (is_null($this->lastExtension)) {
-                $this->lastExtension = false;
-            }
-        }
-        return $this->lastExtension;
-    }
-
-    public function lastExtensionDate()
-    {
-        $lastExtension = $this->getlastExtension();
-        if ($lastExtension === false) {
-            return '';
-        }
-        return DateHelper::create($lastExtension->createdAt())->translateToShortDate();
-    }
-
-    public function lastExtensionAmount()
-    {
-        $lastExtension = $this->getlastExtension();
-        if ($lastExtension === false) {
-            return '';
-        }
-        return $this->toMoney($lastExtension->amount());
-    }
-
-    public function lastExtensionDateDiff()
-    {
-        $lastExtension = $this->getlastExtension();
-        if ($lastExtension === false) {
-            return '';
-        }
-        return DateHelper::getDifference($lastExtension->createdAt())->forHumans();
-    }
-
-    /**
-     * Presents a good looking view for months statistics.
-     */
-    public function monthStatistics($small = false)
-    {
-        //TODO Refactor and find right place
-        $monthsElapsed = $this->entity->elapsedMonths();
-        $monthsExtended = $this->entity->extendedMonths();
-        $months = $this->entity->months();
-
-        // First shows the transcurred months
-        $statistics[] = ['class' => 'default', 'months' => $monthsElapsed];
-        // number of months of the contract
-        $statistics[] = ['class' => 'default', 'months' => $months];
-
-        // Look for the appropiate color clasification for the extended months
-        $parameter = ($monthsExtended + 1) - $monthsElapsed;
-        if ($parameter < 0) {
-            $class = 'danger';
-        } elseif ($parameter > 1) {
-            $class = 'success';
-        } else {
-            $class = 'warning';
-        }
-        $statistics[] = ['class' => $class, 'months' => $monthsExtended];
-
-        // Look for the appropiate color clasification for the remaining
-        $parameter = ($months + $monthsExtended) - $monthsElapsed;
-        if ($parameter <= 0) {
-            $class = 'danger';
-        } elseif ($parameter > ceil($months * 0.5)) {
-            $class = 'success';
-        } else {
-            $class = 'warning';
-        }
-        $statistics[] = ['class' => $class, 'months' => $parameter];
-
-        if ($small) {
-            $btnType = 'btn-sm';
-            $btnIcon = '';
-        } else {
-            $btnType = 'btn btn-lg';
-            $btnIcon = '<i class="fa fa-calendar-o"></i> ';
-        }
-
-        $return = '';
-        $tooltips = [
-            'Meses transcurridos',
-            'Meses del contrato',
-            'Meses abonados',
-            'Meses restantes',
-        ];
-        foreach ($statistics as $index => $data) {
-            $return .= '<span data-toggle="tooltip" data-original-title="' . $tooltips[$index] . '" class="tooltips ' . $btnType . ' btn-' . $data['class'] . '">' . $btnIcon . $data['months'] . "</span>\n";
-        }
-
-        return $return;
     }
 
     public function profit()
